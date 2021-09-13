228e70a28933a4c7b7a102bb920db0502e69bb7b
kchyzhyk <kchyzhykova888@gmail.com>
2021-09-13 20:19:15 +0200

    +update

--- App.js
+++ App.js
@@ -1,18 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, StyleSheet, View} from 'react-native';
 import {Navbar} from "./src/Navbar";
 import {AddTodo} from "./src/AddTodo";
+import {Todo} from "./src/Todo";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Navbar title='Todo App!'/>
-      <AddTodo />
-    </View>
-  );
+    const [todos, setTodos] = useState([
+        {id: 1, title: 'test'},
+        {id: 2, title: 'test'},
+        {id: 3, title: 'test'},
+        {id: 4, title: 'test'},
+        {id: 5, title: 'test'},
+        {id: 6, title: 'test'},
+        {id: 7, title: 'test'},
+        {id: 8, title: 'test'},
+        {id: 9, title: 'test'},
+        {id: 10, title: 'test'},
+        {id: 11, title: 'test'},
+        {id: 12, title: 'test'}
+    ])
+
+    const addTodo = (title) => {
+        setTodos(prev => [...prev,
+            {
+                id: Date.now().toString(),
+                title
+            }])
+    }
+
+    return (
+        <View>
+            <Navbar title='Todo App!'/>
+            <View style={styles.container}>
+                <AddTodo onSubmit={addTodo}/>
+
+                <FlatList
+                    keyExtractor={item => item.id.toString()}
+                    data={todos}
+                    renderItem={({item}) => <Todo todo={item}/>}
+                />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-  },
+    container: {
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+    }
 });
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import {View, StyleSheet, TextInput, Button} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, TextInput, View, Alert} from 'react-native';
 
-export const AddTodo = props => {
+export const AddTodo = ({onSubmit}) => {
+
+    const [value, setValue] = useState('')
+
+    const pressHandler = () => {
+        if (value.trim()) {
+            onSubmit(value)
+            setValue('')
+        } else {
+           Alert.alert('Название не может быть пустым')
+        }
+    }
+
     return (
         <View style={styles.block}>
-            <TextInput style={styles.input}/>
-            <Button title='Добавить'/>
+            <TextInput style={styles.input}
+                       onChangeText={text => setValue(text)}
+                       value={value}
+                       placeholder='Введите...'
+                       autoCorrect={false}
+                       autoCapitilize='none'
+            />
+            <Button title='Добавить' onPress={pressHandler}/>
         </View>
     )
 }
@@ -13,10 +31,16 @@ export const AddTodo = props => {
 const styles = StyleSheet.create({
     block: {
         flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 15
     },
     input: {
         width: '70%',
+        padding: 10,
         borderStyle: 'solid',
+        borderBottomWidth: 2,
+        borderBottomColor: '#3949ab'
+    },
 
-    }
 })
