76acf6f37d72b372e58ee6336306fffadd965fbb
kchyzhyk <kchyzhykova888@gmail.com>
2021-10-09 10:19:17 +0300

    +custom navbar theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1
     },
     text:{
-        color: 'white',
+        color: Platform.OS === 'ios' ? THEME.MAIN_COLOR : '#fff' ,
         fontSize: 20
     }
 })
