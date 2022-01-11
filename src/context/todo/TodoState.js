2b2765eefd608e19297810749819dfe2937c51fd
kchyzhyk <kchyzhykova888@gmail.com>
2022-01-11 22:49:15 +0200

    +upd

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,21 +1,30 @@
 import React, { useReducer, useContext } from "react";
 import {TodoContext} from './todoContext'
 import {todoReducer} from "./todoReducer";
-import {ADD_TODO, REMOVE_TODO, UPDATE_TODO} from "../types";
+import {ADD_TODO, HIDE_LOADER, REMOVE_TODO, SHOW_ERROR, SHOW_LOADER, UPDATE_TODO} from "../types";
 import {ScreenState} from "../screen/ScreenState";
 import {ScreenContext} from "../screen/screenContext";
 import {Alert} from "react-native";
 
 export const TodoState = ({ children }) => {
     const initialState = {
-        todos: [
-            {id: '1', title: 'Выучить React Native'},
-            {id: '2', title: 'second task'}]
+        todos: [],
+        loading: false,
+        error: null
     }
     const {changeScreen} = useContext(ScreenContext)
     const [state, dispatch] = useReducer(todoReducer, initialState)
 
-    const addTodo = title => dispatch({type: ADD_TODO, title})
+    const addTodo = async title => {
+        const response = await fetch('https://rn-appka-default-rtdb.firebaseio.com/todos.json', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({title})
+        })
+        const data = await response.json()
+        console.log('DATA', data)
+        dispatch({type: ADD_TODO, title})
+    }
 
     const removeTodo = id => {
         const todo = state.todos.find(t => t.id === id )
@@ -41,6 +50,14 @@ export const TodoState = ({ children }) => {
     }
 
     const updateTodo = (id, title) => dispatch({type: UPDATE_TODO, id, title})
+
+    const showLoader = () => dispatch({type: SHOW_LOADER})
+
+    const hideLoader = () => dispatch({type: HIDE_LOADER})
+
+    const showError = error => dispatch({type: SHOW_ERROR, error })
+
+    const clearError = error => dispatch({type: CLEAR_ERROR})
 
     return <TodoContext.Provider
         value={{
